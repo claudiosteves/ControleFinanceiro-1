ef4a058952a5759445561038d3b1a25a6e05df03
James Moreira <jamesmsjr@gmail.com>
2018-06-07 21:51:40 -0300

    Implementação do Rapid Floating Action Button e DatePickerDialog

--- app/src/main/java/senac/controlefinanceiro/ListActivity.java
+++ app/src/main/java/senac/controlefinanceiro/ListActivity.java
@@ -40,37 +40,20 @@ public class ListActivity extends AppCompatActivity implements RapidFloatingActi
             rfaContent.setOnRapidFloatingActionContentLabelListListener(this);
             List<RFACLabelItem> items = new ArrayList<>();
             items.add(new RFACLabelItem<Integer>()
-                    .setLabel("Github: wangjiegulu")
-                    .setResId(R.mipmap.ic_launcher)
-                    .setIconNormalColor(0xffd84315)
-                    .setIconPressedColor(0xffbf360c)
+                    .setLabel("Adicionar Despesa")
+                    .setResId(R.drawable.ic_despesa)
+                    .setIconNormalColor(Color.RED)
+                    .setIconPressedColor(Color.GRAY)
                     .setWrapper(0)
             );
             items.add(new RFACLabelItem<Integer>()
-                    .setLabel("tiantian.china.2@gmail.com")
+                    .setLabel("Adicionar Receita")
                     .setResId(R.mipmap.ic_launcher)
-                    .setIconNormalColor(0xff4e342e)
-                    .setIconPressedColor(0xff3e2723)
-                    .setLabelColor(Color.WHITE)
-                    .setLabelSizeSp(14)
+                    .setIconNormalColor(Color.WHITE)
+                    .setIconPressedColor(Color.GRAY)
                     .setWrapper(1)
             );
-            items.add(new RFACLabelItem<Integer>()
-                    .setLabel("WangJie")
-                    .setResId(R.mipmap.ic_launcher)
-                    .setIconNormalColor(0xff056f00)
-                    .setIconPressedColor(0xff0d5302)
-                    .setLabelColor(0xff056f00)
-                    .setWrapper(2)
-            );
-            items.add(new RFACLabelItem<Integer>()
-                    .setLabel("Compose")
-                    .setResId(R.mipmap.ic_launcher)
-                    .setIconNormalColor(0xff283593)
-                    .setIconPressedColor(0xff1a237e)
-                    .setLabelColor(0xff283593)
-                    .setWrapper(3)
-            );
+
             rfaContent
                     .setItems(items)
                     .setIconShadowColor(0xff888888)
@@ -88,15 +71,25 @@ public class ListActivity extends AppCompatActivity implements RapidFloatingActi
         }
     }
 
-    @Override
-    public void onRFACItemLabelClick(int position, RFACLabelItem item) {
-        Toast.makeText(getApplicationContext(), "clicked label: " + position, Toast.LENGTH_SHORT).show();
+    private void chamarAtividades(int position){
+        switch (position){
+            case 0:
+                startActivity(new Intent(this, DespesaActivity.class));
+                break;
+            case 1:
+                startActivity(new Intent(this, ReceitaActivity.class));
+                break;
+        }
         rfabHelper.toggleContent();
     }
 
     @Override
+    public void onRFACItemLabelClick(int position, RFACLabelItem item) {
+        chamarAtividades(position);
+    }
+
+    @Override
     public void onRFACItemIconClick(int position, RFACLabelItem item) {
-        Toast.makeText(getApplicationContext(), "clicked icon: " + position, Toast.LENGTH_SHORT).show();
-        rfabHelper.toggleContent();
+        chamarAtividades(position);
     }
 }
--- app/src/main/java/senac/controlefinanceiro/ReceitaActivity.java
+++ app/src/main/java/senac/controlefinanceiro/ReceitaActivity.java
@@ -1,13 +1,21 @@
 package senac.controlefinanceiro;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class ReceitaActivity extends AppCompatActivity {
+
+    EditText dataReceita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,31 @@ public class ReceitaActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        dataReceita = findViewById(R.id.data_receita);
+    }
+
+    public void calendario(View view) {
+        try {
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            dataReceita.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        } catch (Exception e){
+            Log.e("Receita", e.getMessage());
+        }
     }
 }
