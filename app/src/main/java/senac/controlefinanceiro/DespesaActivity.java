c57baeeb1a0c59f350cbe380240baf647c8db064
James Moreira <jamesmsjr@gmail.com>
2018-06-08 20:31:10 -0300

    Implementação do DatePickerDialog na Despesa.

--- app/src/main/java/senac/controlefinanceiro/DespesaActivity.java
+++ app/src/main/java/senac/controlefinanceiro/DespesaActivity.java
@@ -1,13 +1,23 @@
 package senac.controlefinanceiro;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class DespesaActivity extends AppCompatActivity {
+
+    EditText valorDespesa;
+    EditText dataDespesa;
+    EditText descricaoDespesa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +35,33 @@ public class DespesaActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        valorDespesa = findViewById(R.id.valor_despesa);
+        dataDespesa = findViewById(R.id.data_despesa);
+        descricaoDespesa = findViewById(R.id.descricao_despesa);
+    }
+
+    public void calendario(View view) {
+        try {
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            dataDespesa.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        } catch (Exception e){
+            Log.e("Despesa", e.getMessage());
+        }
     }
 }
